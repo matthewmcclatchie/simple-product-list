f03dd9bfa626daaa52b4c52b192506f52fddac47
Matthew Mcclatchie <matthewmcclatchie@me.com>
2018-11-12 15:18:05 +1100

    feat(ArticleCard): Setup basic structure using styled-components
    This will definitely change, just setting this up to being working with nested components.

--- components/molecules/ArticleCard/index.js
+++ components/molecules/ArticleCard/index.js
@@ -3,15 +3,31 @@
  */
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+/**
+ * Styled Components
+ */
+const Wrap = styled.article`
+  border: 1px solid red;
+`;
+
+const Header = styled.header`
+  border: 1px solid blue;
+`;
 
 /**
  * Component
  * @param {object} props
  */
 const ArticleCard = ({ text }) => (
-  <div>
-    <p>{text}</p>
-  </div>
+  <Wrap>
+
+    <Header>
+      <p>{text}</p>
+    </Header>
+
+  </Wrap>
 );
 
 
